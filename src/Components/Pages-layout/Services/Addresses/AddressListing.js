ab0040f61d8d5f7559b23d2009ddfd498613b277
aaftab22 <aaftabvhora62@gmail.com>
2023-08-09 14:15:29 -0400

    Manage address functionality

--- src/Components/Pages-layout/Services/Addresses/AddressListing.js
+++ src/Components/Pages-layout/Services/Addresses/AddressListing.js
@@ -1,67 +1,60 @@
 import { useEffect, useState } from "react";
 import './AddressListing.css';
-
+import { get, ref } from "firebase/database"; 
+import { auth, database } from "../../../../firebase";
 const AddressListing = () => {
       
     // const keys = data[0] && Object.keys(data[0]);
-    const keys = ["name", "street", "city", "province", "postalCode"];
+    const keys = ["name", "phone" , "street", "city", "province", "postalCode"];
+    const [addresses, setAddresses] = useState([]);
+    let titles = ["Full Name", "phone" ,"Street", "City", "Province", "Postal Code"];
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const user = auth.currentUser;
+                if (user) {
+                    const userId = user.uid;
+                    const dbRef = ref(database, `addresses/${userId}`);
+                    const snapshot = await get(dbRef);
+                    const fetchedData = snapshot.val();
 
-    let titles = ["Full Name", "Street", "City", "Province", "Postal Code"];
-
-    const data = [
-    {
-        "imageSrc": "Person1",
-        "name": "Zack Daniel",
-        "street": "401, 204 Aurburn Drive",
-        "city": "Waterloo",
-        "province": "Ontario",
-        "postalCode": "N2J 4J1"
-    },
-    {
-        "imageSrc": "Person1",
-        "name": "Jack Daniel",
-        "street": "411, 210 Aurburn Drive",
-        "city": "Waterloo",
-        "province": "Ontario",
-        "postalCode": "N2J 4J2"
-    }
-    ]
+                    if (fetchedData) {
+                        const addressArray = Object.keys(fetchedData).map((key) => ({
+                            ...fetchedData[key],
+                            key: key,
+                        }));
+                        setAddresses(addressArray);
+                    }
+                }
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        fetchData();
+}, [Date.now()]);
+   
       // console.log(keys);
-      console.log(data);   
-       return (
+    return (
         <div className='table-layout'>
-            {data.length > 0 ? (
+            {addresses.length > 0 ? (
                 <table className="table">
                     <thead>
                         <tr>
                             {titles.map((title) => (
-                                <th scope="col" title={title}>
+                                <th scope="col" title={title} key={title}>
                                     {title}
                                 </th>
                             ))}
-                            
                         </tr>
                     </thead>
                     <tbody>
-                        {data.map((x, index) => (
+                        {addresses.map((address, index) => (
                             <tr key={index}>
                                 {keys.map((key) => (
                                     <td key={key}>
-                                    {key === 'imageSrc' ? (
-                                      <div className='circle-image'>
-                                        <img
-                                          src={process.env.PUBLIC_URL + '/Images/' + x[key]}
-                                          alt={x[key]}
-                                        />
-                                      </div>
-                                    ) : key === 'skills' ? (
-                                      x[key] ? x[key] : 'N/A' // Display "N/A" if skills are empty
-                                    ) : (
-                                      x[key]
-                                    )}
-                                  </td>
+                                        {address[key]}
+                                    </td>
                                 ))}
-                                
                             </tr>
                         ))}
                     </tbody>
